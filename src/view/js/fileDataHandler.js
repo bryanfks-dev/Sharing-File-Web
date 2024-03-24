b0608efdd42452b7cf2f42050ee17e17ba684583
bryanfks-dev <bryanfernandoks11@gmail.com>
2024-03-24 11:25:02 +0700

    change a link target

--- src/view/js/fileDataHandler.js
+++ src/view/js/fileDataHandler.js
@@ -4,7 +4,7 @@ const fileData = (num, name, link) => {
             <td>${num}</td>
             <td>${name}</td>
             <td>
-                <a href="/file/${name}" targer="_blank">
+                <a href="/file/${name}" target="_blank">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: var(--green-clr-100);"><path d="M19 9h-4V3H9v6H5l7 8zM4 19h16v2H4z"></path></svg>
                 </a>
             </td>
